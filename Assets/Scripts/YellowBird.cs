7cdb0b067776b73c31ae9c58251739a9f1783976
husainyusuf382@gmail.com <husainyusuf382@gmail.com>
2021-09-21 08:19:01 +0700

    Add project files.

---
+++ Assets/Scripts/YellowBird.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class YellowBird : Bird
+{
+    [SerializeField]
+    public float _boostForce = 100;
+    public bool _hasBoost = false;
+
+    public void Boost()
+    {
+        if (State == BirdState.Thrown && !_hasBoost)
+        {
+            RigidBody.AddForce(RigidBody.velocity * _boostForce);
+            _hasBoost = true;
+        }
+    }
+
+    public void OnTap()
+    {
+        Boost();
+    }
+}
